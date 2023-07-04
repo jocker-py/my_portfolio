457592f288124b3fad7b68a84a087fd9df20b404
jocker-py <buladzislau@gmail.com>
2023-07-04 22:46:58 +0300

    refactor: move MySkills and Skills to About

---
+++ src/components/About/MySkills/MySkills.tsx
@@ -0,0 +1,25 @@
+// import React from "react";
+// import s from "./MySkills.module.scss";
+// import Skill from "./Skill/Skill";
+// import Container from "../common/Container/Conatiner";
+// import Title from "../common/Title/Title";
+//
+const MySkills = () => {
+  return (
+    <section id="skills">
+      {/*<Container>*/}
+      {/*  <div className={s.wrapper}>*/}
+      {/*    <Title>Мои скиллы</Title>*/}
+      {/*    <ul className={s.list}>*/}
+      {/*      <Skill/>*/}
+      {/*      <Skill/>*/}
+      {/*      <Skill/>*/}
+      {/*    </ul>*/}
+      {/*  </div>*/}
+      {/*</Container>*/}
+
+    </section>
+  );
+};
+//
+export default MySkills;
