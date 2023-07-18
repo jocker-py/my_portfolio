dd85136dd75083f8d14e9df812add10ea2be25e9
jocker-py <buladzislau@gmail.com>
2023-07-18 13:58:18 +0300

    refactor: rename className

--- src/components/About/MySkills/Skill/Skill.tsx
+++ src/components/About/MySkills/Skill/Skill.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from "react";
-import s from './Skill.module.scss';
+import s from "./Skill.module.scss";
 
 type SkillPropsType = {
   icon: string;
@@ -9,7 +9,7 @@ type SkillPropsType = {
 const Skill: FC<SkillPropsType> = ({icon, title}) => {
   return (
     <li className={s.skill}>
-      <div><img src={icon} alt={title}/></div>
+      <div><img src={icon} className={s.icon} alt={title}/></div>
       <h4 className={s.title}>{title}</h4>
     </li>
   );
