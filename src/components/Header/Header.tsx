07f5d7ce08414de1b4e6d5ee2ca54611fe9686bf
jocker-py <buladzislau@gmail.com>
2023-07-19 12:48:55 +0300

    feat: use NavIconLink instead of a link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,33 @@
 import React from "react";
-import s from "./Header.module.css";
-import Container from "../common/Container/Conatiner";
+import s from "./Header.module.scss";
+import {Endpoints} from "../../config/endpoints";
+import {
+  faHouseChimney,
+  faUser,
+  faBriefcase,
+  faEnvelopeOpenText, faMoon,
+
+} from "@fortawesome/free-solid-svg-icons";
+import {NavIconLink} from "../common/NavIconLink/NavIconLink";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 
 const Header = () => {
   return (
     <header className={s.header}>
-        <Container className={s.container}>
-          <nav className={s.nav}>
-            <a href={"#main"}>Главная</a>
-            <a href={"#skills"}>Навыки</a>
-            <a href={"#projects"}>Проекты</a>
-            <a href={"#contacts"}>Контакты</a>
-          </nav>
-        </Container>
+      <div className={s.wrapper}>
+        <div className={s.themeSwitcher}>
+          <FontAwesomeIcon icon={faMoon} size="xs" style={{fontSize: '28px'}}/>
+        </div>
+        <nav className={s.nav}>
+          <NavIconLink to={Endpoints.Home} title={"HOME"} icon={faHouseChimney}/>
+          <NavIconLink to={Endpoints.About} title={"ABOUT"} icon={faUser}/>
+          <NavIconLink to={Endpoints.Portfolio} title={"PORTFOLIO"} icon={faBriefcase}/>
+          <NavIconLink to={Endpoints.Contact} title={"CONTACT"} icon={faEnvelopeOpenText}/>
+        </nav>
+      </div>
     </header>
-
   );
 };
 
