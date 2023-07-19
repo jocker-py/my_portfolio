44c47358b55b5d24807c78274159fbc6ed71c000
jocker-py <buladzislau@gmail.com>
2023-07-19 19:06:40 +0300

    feat: add props for Button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,16 +1,24 @@
 import React, {ButtonHTMLAttributes, DetailedHTMLProps, FC} from "react";
 import s from "./Button.module.scss";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement>
 
 type ButtonPropsType = DefaultButtonPropsType & {
-  className?: string;
+  text: string;
+  icon: IconProp;
 }
 
-const Button: FC<ButtonPropsType> = ({className, children, ...other}) => {
-  return <button className={className ? s.button + " " + className : s.button}
-                 {...other}>{children}</button>;
+const Button: FC<ButtonPropsType> = ({className, text, icon, ...other}) => {
+  const finalClassName = className ? s.button + " " + className : s.button;
+  return <button className={finalClassName}{...other}>
+    <span className={s.text}>{text}</span>
+    <span className={s.icon}>{
+      <FontAwesomeIcon icon={icon} size="lg" className={s.icon} style={{backgroundColor: '#ffb400'}}/>
+    }</span>
+  </button>;
 };
 
 export default Button;
