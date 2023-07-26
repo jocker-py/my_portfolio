2287e773e40666cef6521e71616fa1627cb8e174
jocker-py <buladzislau@gmail.com>
2023-07-26 22:43:31 +0300

    feat: add ContactForm

--- src/components/Contacts/ContactForm/ContactForm.tsx
+++ src/components/Contacts/ContactForm/ContactForm.tsx
@@ -1,19 +1,80 @@
 import React from "react";
 import s from "./ContactForm.module.scss";
 import Button from "../../common/Button/Button";
-import {faTelegram} from "@fortawesome/free-brands-svg-icons/faTelegram";
+import axios, {AxiosResponse} from "axios";
+import {Form, Formik, Field, ErrorMessage, FormikHelpers} from "formik";
+import {handleValidation} from "./validation";
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons";
+
+export type FormValuesType = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+const CHAT_ID = "484619169";
+const TOKEN = "1762106448:AAFfqFa9km1qtRmeJivw2Jy4D3umshfNA3k";
 
 const ContactForm = () => {
+  const handleSubmit = async (values: FormValuesType, {setSubmitting}: FormikHelpers<FormValuesType>) => {
+    const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+    const message = `
+      Name: ${values.name}
+      Subject: ${values.subject}
+      Email: ${values.email}
+      Message: ${values.message}
+    `;
+    const data = {text: message, chat_id: CHAT_ID};
+    try {
+      const response = await axios.post<AxiosResponse>(url, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if(response.status === 200) alert("Спасибо за ваше сообщение! Скоро отвечу");
+    } catch (error) {
+      alert(`Error: ${error} `);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <form action="#" className={s.form}>
-      <div className={s.wrapper}>
-        <input type="text" className={s.name} placeholder="Your name"/>
-        <input type="phone" className={s.phone} placeholder="Your phone"/>
-      </div>
-      <input type="subject" className={s.subject} placeholder="Your subject"/>
-      <textarea className={s.textarea} rows={8} placeholder="Your message"/>
-      <Button text={'Send Message'} icon={faTelegram}/>
-    </form>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      }}
+      validate={handleValidation}
+      onSubmit={handleSubmit}
+    >
+      {({isSubmitting}) => (
+        <Form className={s.form}>
+          <div className={s.wrapper}>
+            <div className={s.fieldWrapper}>
+              <Field type="text" name="name" placeholder="Your name" className={s.name} />
+              <ErrorMessage name="name" component="div" className={s.errorMessage}/>
+            </div>
+            <div className={s.fieldWrapper}>
+              <Field type="email" name="email" className={s.email} placeholder="Your email" />
+              <ErrorMessage name="email" component="div" className={s.errorMessage}/>
+            </div>
+          </div>
+          <div className={s.fieldWrapper}>
+            <Field type="text" name="subject" placeholder="Your subject" className={s.subject} />
+            <ErrorMessage name="subject" component="div" className={s.errorMessage}/>
+          </div>
+          <div className={s.fieldWrapper}>
+            <Field type="text" name="message" placeholder="Your message" rows={8} className={s.textarea} />
+            <ErrorMessage name="message" component="div" className={s.errorMessage}/>
+          </div>
+          <Button text="Send Message" icon={faPaperPlane} disabled={isSubmitting}/>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
