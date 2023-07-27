cd73e0500c23119a6d5e554173143eccf5225f1d
jocker-py <buladzislau@gmail.com>
2023-07-27 20:41:03 +0300

    refactor: change example to my email

--- src/features/Contacts/Contacts.tsx
+++ src/features/Contacts/Contacts.tsx
@@ -1,13 +1,10 @@
 import React from "react";
+import {v1} from "uuid";
 import s from "./Contacts.module.scss";
-import HeadTitle from "../HeadTitle/HeadTitle";
 import {ContactItem, ContactItemType} from "./ContactItem/ContactItem";
 import {faMap} from "@fortawesome/free-solid-svg-icons/faMap";
 import {faEnvelopeOpen} from "@fortawesome/free-solid-svg-icons/faEnvelopeOpen";
 import {faPhoneSquare} from "@fortawesome/free-solid-svg-icons";
-import List from "../common/List/List";
-import {v1} from "uuid";
-
 import ContactLink from "./ContactLink/ContactLink";
 import ContactForm from "./ContactForm/ContactForm";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
@@ -16,6 +13,8 @@ import {faTelegram} from "@fortawesome/free-brands-svg-icons/faTelegram";
 import {faInstagram} from "@fortawesome/free-brands-svg-icons/faInstagram";
 import {faViber} from "@fortawesome/free-brands-svg-icons/faViber";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons/faEnvelope";
+import HeadTitle from "../../components/HeadTitle/HeadTitle";
+import List from "../../components/List/List";
 
 
 const Contacts = () => {
@@ -65,19 +64,17 @@ const Contacts = () => {
     }, {
       id: v1(),
       icon: faEnvelope,
-      link: "mailto:mail@example.com",
+      link: "mailto:buladzislau@gmail.com",
     }];
 
   return (
     <section className={s.container}>
       <HeadTitle background={"Contacts"} title={"Get In"} subtitle={"Touch"}/>
-
       <article className={s.wrapper}>
         <List items={contacts} renderItem={(contact) => <ContactItem {...contact} />} className={s.contactsList}/>
         <List items={societies} renderItem={(society) => <ContactLink key={society.id} {...society}/>} className={s.societiesList}/>
       </article>
       <ContactForm/>
-
     </section>
   );
 };
