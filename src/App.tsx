8b6fb04403cd4a8509b5d2abfb60344f7534b98d
jocker-py <buladzislau@gmail.com>
2023-07-30 20:40:37 +0300

    refactor: remove unused div

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import s from './App.module.scss';
+import React from "react";
 import Router from "./components/Router/Router";
 import Header from "./components/Header/Header";
 import {BrowserRouter} from "react-router-dom";
 
-
 function App() {
   return (
     <BrowserRouter>
-      <div className={s.app}>
-        <Header/>
-        <Router/>
-      </div>
+      <Header/>
+      <Router/>
     </BrowserRouter>
   );
 }
