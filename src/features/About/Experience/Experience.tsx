d1913a9022f588b6992bb1a1d9344cb175a7730a
jocker-py <buladzislau@gmail.com>
2023-07-30 20:35:11 +0300

    refactor: replace items

--- src/features/About/Experience/Experience.tsx
+++ src/features/About/Experience/Experience.tsx
@@ -12,19 +12,20 @@ const Experience = () => {
       img: "",
       name: "Belarusian nuclear power plant",
       link: "https://belaes.by/en/",
-      position: "Turbine equipment operator of 7 degree",
+      position: "Turbine equipment operator of 8 degree",
       description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      year: "2018-2022",
+      year: "2022-present",
     },
     {
       id: v1(),
       img: "",
       name: "Belarusian nuclear power plant",
       link: "https://belaes.by/en/",
-      position: "Turbine equipment operator of 8 degree",
+      position: "Turbine equipment operator of 7 degree",
       description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      year: "2022-present",
+      year: "2018-2022",
     },
+
   ];
   return (
     <article className={s.wrapper}>
